b34ec794fbf7255f482b939ece0ee3514a48c93e
ngranzow <nate.granzow@gmail.com>
2022-03-20 17:32:57 -0500

    setup connection.js

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
+    },
+    console.log('Connection to the employee database.')
+);
+
+module.exports = db;
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const inquier = require('inquirer');
 const cTable = require('console.table');
+const db = require('./db/connection');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3006,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: 'employee_DB',
+const PORT = process.env.PORT || 3001;
+
+connection.connect (err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    mainMenu();
 });
 
