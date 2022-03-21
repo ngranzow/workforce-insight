745cb858cc6a0e484fe7273b4ca0dcfdd5d6ea8e
ngranzow <nate.granzow@gmail.com>
2022-03-21 00:52:59 -0500

    can now view by department, role, employee and add department, role, employee

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const mainMenu = () => {
             'Delete a role',
             'Delete an employee',
             'View department budgets',
-            'Do nothing']
+            'Exit']
         }
     ])
     .then((menuAnswers) => {
@@ -110,3 +110,187 @@ const viewAll = (table) => {
         mainMenu();
     });
 };
+
+addDep = () => {
+    inquier.prompt([
+        {
+            type: 'input',
+            name: 'depName',
+            message: 'What is the name of the department you would like to add?',
+            validate: addDep => {
+                if (addDep) {
+                    return true;
+                } else {
+                    console.log(' Please enter department name.');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(depAnswer => {
+        const sql = `INSERT INTO department (name) VALUES (?)`;
+        db.query(sql, [depAnswer.depName], (err, res) => {
+            if (err) throw err;
+            console.log(`Added ${depAnswer.depName} to departments!`);
+
+            viewAll('DEPARTMENT');
+        });
+    });
+};
+
+addRol = () => {
+    const departments = []
+    db.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+
+        res.forEach(dep => {
+            let depObj = {
+                name: dep.name,
+                value: dep.id
+            }
+            departments.push(depObj);
+        })
+    })
+
+    inquier.prompt([
+        {
+            type: 'input',
+            name: 'newRole',
+            message: 'What is the name of the new role?',
+            validate: newRole => {
+                if (newRole) {
+                    return true;
+                } else {
+                    console.log(' Please enter name of role.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input', 
+            name: 'newSalary',
+            message: "What is the salary of the new role?",
+            validate: newSalary => {
+                if (!isNaN()) {
+                    console.log(' Please enter the salary.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "departments",
+            choices: departments,
+            message: "which department is this role in?"
+        }
+    ])
+    .then(rolAnswer => {
+        const params = [rolAnswer.newRole, rolAnswer.newSalary, rolAnswer.departments];
+        const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+        db.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.log(`Added ${rolAnswer.newRole} to roles!`)
+
+            viewAll('ROLE');
+        });
+    });
+};
+
+addEmp = () => {
+    const roles = [];
+    const employees = [
+        {
+            name: 'None',
+            value: null
+        }
+    ];
+
+    db.query("SELECT * FROM role", (err, res) => {
+        if (err) throw err;
+        
+        res.forEach(({ title, id }) => {
+            roles.push({
+                name: title,
+                value: id
+            });
+        });
+    });
+
+    db.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err;
+        
+        res.forEach(({ first_name, last_name, id }) => {
+            employees.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
+        });
+    }); 
+
+    inquier.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: "What is the new employee's first name?",
+            validate: firstName => {
+              if (firstName) {
+                  return true;
+              } else {
+                  console.log('Please enter a first name.');
+                  return false;
+              }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "What is the new employee's last name?",
+            validate: lastName => {
+              if (lastName) {
+                  return true;
+              } else {
+                  console.log('Please enter a last name.');
+                  return false;
+              }
+            }
+        },
+        {
+            type: "list",
+            name: "roleId",
+            choices: roles,
+            message: "What is the new employee's role?"
+        },
+        {
+            type: "list",
+            name: "managerId",
+            choices: employees,
+            message: "Who is the new employee's manager?"
+        }
+    ])
+    .then(empAnswer => {
+        const params = [empAnswer.firstName, empAnswer.lastName, empAnswer.roleId, empAnswer.managerId];
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        db.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.log(`Added ${empAnswer.firstName} ${empAnswer.lastName} to employees!`)
+
+            viewAll('EMPLOYEE');
+        });
+    });
+};
+
+upEmp = () => {
+
+}
+
+upMan = () => {}
+
+viewEByD = () => {}
+
+delDep = () => {}
+
+delRol = () => {}
+
+viewBud = () => {}
